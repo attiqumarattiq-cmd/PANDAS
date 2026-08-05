d9eb1fe103ba589a6160b276adda5c2f0ebb62e5
Umar <attiqumarattiq@gmail.com>
2026-08-05 23:47:47 +0500

    Updated

--- find_the_string_name_in_data.py
+++ find_the_string_name_in_data.py
@@ -4,4 +4,4 @@ df = pd.read_csv('pokemon_data.csv')
 
 print(  df.loc[df['Name'].str.contains('Mega')]  )     # shows that contain 'Mega'
 
-print(  df.loc[~df['Name'].str.contains('Mega')] )      # shows that doenot contain 'Mega'
+print(df.loc[~df['Name'].str.contains('Mega')] )      # shows that doenot contain 'Mega'
