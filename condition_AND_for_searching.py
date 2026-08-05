338c832804161f95702ca112141c17a68cad71ce
Umar <attiqumarattiq@gmail.com>
2026-08-05 23:46:44 +0500

    Updated

--- condition_AND_for_searching.py
+++ condition_AND_for_searching.py
@@ -2,7 +2,7 @@
 import pandas as pd
 df  = pd.read_csv('pokemon_data.csv')
 
-print(df.loc[ (df['Type 1'] == 'Normal')   &  (df['Type 2'] == 'Flying')  ])
+print(df.loc[(df['Type 1'] == 'Normal')   &  (df['Type 2'] == 'Flying')  ])
 
 ## It prints that row in which both condition matches 
                       # means in that column where "Type 1" is "Normal" and "Type 2" is "Flying"
